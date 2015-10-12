4d29c0c28d06947211c41f545fd9e75210b03b80
renkanai <turutekab@gmail.com>
2015-10-12 22:53:41 +0900

    WIP

---
+++ src/mlp/main.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+
+__author__ = 'ren'
+
+
+def data():
+    return None, None
+
+
+if __name__ == '__main__':
+    """
+    DATA
+    """
+    train, test = data()
+
+    """
+    MODEL
+    """
+
+
+    """
+    TRAIN
+    """
+
+
