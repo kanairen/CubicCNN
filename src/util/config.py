10e31ea2848bcc5e410603c0a4d60a96255dddf2
kanairen <turutekab@gmail.com>
2015-10-13 13:21:46 +0900

    add config.pyを追加
    PATHなどの静的な設定を記述するconfigモジュールを追加

---
+++ src/util/config.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+
+import os
+
+__author__ = 'ren'
+
+"""
+PATH
+"""
+
+path = os.path.dirname(__file__)
+
+path_res = path + "/res"
+
+# res
+path_res_2d = path + "/2d"
+path_res_3d = path + "/3d"
+
+# res/2d
+path_res_2d_pattern = path_res_2d + "/pattern"
+
+# res/3d
+path_res_3d_psb = path_res_3d + "/psb"
+path_res_3d_psb_classifier = path_res_3d_psb + "/classifier"
