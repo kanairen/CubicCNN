f3e5f78de031a80cb24dfdcdf2b445f722a6ad8a
kanairen <turutekab@gmail.com>
2015-10-17 18:20:55 +0900

    modify main::cubic_cnn()テストデータの切り替え追加

--- src/main.py
+++ src/main.py
@@ -11,27 +11,33 @@ from src.util.config import path_res_2d_pattern
 
 __author__ = 'ren'
 
+
 # TODO バッチ分割実装 15min
-# TODO テストデータシャッフル 15min
-# TODO データ切り替え 15min
+# TODO テストデータシャッフル 15min(18:35)
 # TODO 誤りデータの表示と誤り例（深層学習　p）1h
 # TODO Dropoutの実装 1h
 
+# TODO CNN
+# TODO フィルタ実装
+# TODO プレーンフィルタ実装
+# TODO スポークフィルタ実装
+
 
 @client
-def cubic_cnn(n_div=50):
-
+def cubic_cnn(n_div=50, is_boxel=False):
     """
     DATA
     """
     print "loading data..."
-    # # 点群データ
-    # train_inputs, train_answers = PSB.load_boxels(is_test=False)
-    # test_inputs, test_answers = PSB.load_boxels(is_test=True)
-    inputs, answers, images, r_images = Image.image_set(path_res_2d_pattern,
-                                                        Image.TRANS)
-    train_inputs, test_inputs, train_answers, test_answers, perm = Image.hold_out(
-        inputs, answers, train_rate=0.8)
+    if is_boxel:
+        # 点群データ
+        train_inputs, train_answers = PSB.load_boxels(is_test=False)
+        test_inputs, test_answers = PSB.load_boxels(is_test=True)
+    else:
+        inputs, answers, images, r_images = Image.image_set(path_res_2d_pattern,
+                                                            Image.TRANS)
+        train_inputs, test_inputs, train_answers, test_answers, perm = Image.hold_out(
+            inputs, answers, train_rate=0.8)
 
     train_inputs = [inputs.flatten() for inputs in train_inputs]
     test_inputs = [inputs.flatten() for inputs in test_inputs]
@@ -43,19 +49,16 @@ def cubic_cnn(n_div=50):
     print "test data : ", len(test_inputs)
     print "train classes : ", len(set(train_answers))
     print "test classes : ", len(set(test_answers))
-    # print "total classes : ", len(set(train_answers + test_answers))
 
     """
     MODEL
     """
 
     print "preparing models..."
-    #
-    # model = MLP(l1=Layer(n_div ** 3, 2000),
-    #             l2=Layer(2000, 1000),
-    #             l3=Layer(1000, 500))
 
-    model = MLP(l1=Layer(128 * 128, 2000),
+    n_in = n_div ** 3 if is_boxel else 128 * 128
+
+    model = MLP(l1=Layer(n_in, 2000),
                 l2=Layer(2000, 1000),
                 l3=Layer(1000, 500))
     model.chain()
@@ -65,7 +68,7 @@ def cubic_cnn(n_div=50):
     """
     train_accuracies = []
     test_accuracies = []
-    for i in range(1000):
+    for i in range(10000):
         print "{}st learning...".format(i)
         train_accuracy = model.forward(inputs=train_inputs,
                                        answers=train_answers)
