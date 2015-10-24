f14f1165f13464b49ee0c0fba55cf265390ff475
kanairen <turutekab@gmail.com>
2015-10-24 15:54:39 +0900

    add 畳み込みレイヤ用Utirityモジュール追加

--- src/model/mlp/convolution.py
+++ src/model/mlp/convolution.py
@@ -1,8 +1,9 @@
 # coding:utf-8
 
 import numpy as np
-from theano import config, shared, tensor as T
+from theano import config, shared, tensor as T, function, pp
 from layer import Layer, rnd
+from src.util import conv
 
 __author__ = 'ren'
 
@@ -10,36 +11,32 @@ __author__ = 'ren'
 # TODO ConvLayerTest 30min(12:00)
 # TODO プーリング層
 
-def _pair(x):
-    if hasattr(x, '__getitem__'):
-        return x
-    return (x, x)
 
 
 class ConvLayer2d(object):
     def __init__(self, img_size, k_size, in_channel, out_channel, stride=1,
                  W=None, b=None, dtype=config.floatX, activation=None):
         # 画像サイズ
-        w, h = _pair(img_size)
+        w, h = conv.pair(img_size)
 
         # 入力・出力ユニット数
         self.n_in = w * h * in_channel
         self.n_out = w * h * out_channel
 
         # フィルタサイズ
-        kw, kh = _pair(k_size)
+        kw, kh = conv.pair(k_size)
 
         # フィルタベクトル
         self.h = shared(np.asarray(
             rnd.uniform(low=-np.sqrt(1. / in_channel * kw * kh),
                         high=np.sqrt(1. / in_channel * kw * kh),
-                        size=(kw * kh * in_channel * out_channel)),
+                        size=(out_channel * in_channel * kh * kw)),
             dtype=dtype
         ), name='h', borrow=True)
 
         # 結合行列
         # 値としてスパースなベクトルではなく。ｈのインデックスを保持
-        T = np.zeros(
+        t = np.zeros(
             (self.n_in, self.n_out, out_channel * in_channel * kh * kw),
             dtype=dtype)
 
@@ -49,9 +46,9 @@ class ConvLayer2d(object):
                 for c in range(in_channel * out_channel):
                     for k_h in range(kh):
                         for k_w in range(kw):
-                            T[i + k_w * stride][j + k_h * stride][
+                            t[i + k_w * stride][j + k_h * stride][
                                 (kw * k_h + k_w) * c] = 1
-        self.T = shared(T, name='T', borrow=True)
+        self.t = shared(t, name='t', borrow=True)
 
         # バイアスベクトル
         if b is None:
@@ -60,11 +57,8 @@ class ConvLayer2d(object):
         self.b = b
 
         if activation is None:
-            activation = Layer.relu
+            activation = lambda x: x
         self.activation = activation
-
-        # プーリング関数
-        self.pool = T.max
 
         self.params = self.h, self.b
 
@@ -76,8 +70,6 @@ class ConvLayer2d(object):
 
     def output(self, inputs_symbol):
         # 重み共有のため、毎回フィルタの重みを拝借
-        print self.T.ndim
-        print self.h.ndim
-        W = T.dot(self.T, self.h)
-        print W.ndim
-        return self.activation(T.dot(inputs_symbol, W) + self.b)
+        return self.activation(
+            T.dot(inputs_symbol, T.sum(self.t * self.h, axis=2) + self.b))
+
