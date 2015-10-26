9a54da81958f8f57b5856562c285721fded7e0c9
Ren Kanai <turutekab@gmail.com>
2015-10-26 19:58:14 +0900

    add 数値計算用utilityモジュールを追加
    数値計算用に使うutilityを置く場所としてmath.pyを追加

--- sandbox/src/util/math.py
+++ sandbox/src/util/math.py
@@ -1,3 +1,17 @@
 # coding=utf-8
 
+import functools
+import operator
+
 __author__ = 'kanairen'
+
+
+def product(x):
+    """
+    受け取ったリストの総乗を返す
+    :param x:
+    :return:
+    """
+    prod = functools.partial(functools.reduce, operator.mul)
+    return prod(x)
+
