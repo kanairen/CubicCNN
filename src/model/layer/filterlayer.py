4aab342361b87fcfe24e84c6fe6858583317f9cb
kanairen <turutekab@gmail.com>
2015-11-04 14:13:15 +0900

    fix ベクトル行列T生成処理の修正
    フィルタベクトルhから重みを取り出すためのベクトル行列Tを生成する処理を修正

--- src/model/layer/filterlayer.py
+++ src/model/layer/filterlayer.py
@@ -34,7 +34,7 @@ class FilterLayer(LayerInterface):
         if T is None:
             T = self.init_T(img_w, img_h, kw, kh, sw, sh, n_in, n_out,
                             in_channel, out_channel, dtype=dtype)
-        self.T = T
+        self.T = shared(T, name='T', borrow=False)
 
         # フィルタベクトル
         if h is None:
@@ -78,14 +78,11 @@ class FilterLayer(LayerInterface):
         max_w = img_w - kw
         max_h = img_h - kh
 
-        ksq = kw * kh
-        kcsq = out_channel * ksq
-
         for in_c in six.moves.range(in_channel):
 
-            for in_j in six.moves.range(max_h):
+            for in_j in six.moves.range(img_h):
 
-                for in_i in six.moves.range(max_w):
+                for in_i in six.moves.range(img_w):
 
                     for out_c in six.moves.range(out_channel):
 
@@ -93,15 +90,14 @@ class FilterLayer(LayerInterface):
 
                             for out_i in six.moves.range(0, max_w, sw):
 
-                                j = out_c * img_w * img_h + out_j * img_w + out_i
+                                j = out_c * img_w / sw * img_h / sh + out_j / sh * img_w / sw + out_i / sw
                                 i = in_c * img_w * img_h + in_j * img_w + in_i
 
-                                k_w = out_i - in_i
-                                k_h = out_j - in_j
+                                k_w = in_i - out_i
+                                k_h = in_j - out_j
 
                                 if 0 <= k_w < kw and 0 <= k_h < kh:
-                                    print in_c * kcsq + out_c * ksq + k_h * kw + k_w
                                     T[j][i][
-                                        in_c * kcsq + out_c * ksq + k_h * kw + k_w] = 1.
+                                        out_c * in_channel * kh * kw + in_c * kh * kw + k_h * kw + k_w] = 1.
 
         return T
