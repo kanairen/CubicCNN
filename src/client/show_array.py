e0aade34ed2a6a0181f8ed3a94946af99bbbb0c2
kanairen <turutekab@gmail.com>
2015-11-17 21:27:47 +0900

    fix show_np_array.pyにseabornオプション追加

--- src/client/show_array.py
+++ src/client/show_array.py
@@ -9,7 +9,7 @@ __author__ = 'ren'
 
 
 def show_np_array(x_label, y_label, y_lim, location="lower right", keyword=None,
-                  font_size=15, save=False):
+                  font_size=15, save=False, seaborn=False):
     arrays = {}
     for f in os.listdir(path_res_numpy_array):
         if keyword is not None and keyword not in f:
@@ -18,4 +18,4 @@ def show_np_array(x_label, y_label, y_lim, location="lower right", keyword=None,
         arrays.setdefault(str(f), array)
 
     plot_2d(arrays, x_label, y_label, locate=location, y_lim=y_lim,
-            font_size=font_size, save=save)
+            font_size=font_size, save=save, seaborn=seaborn)
