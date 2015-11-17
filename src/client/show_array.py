3ba54c73c61a732c4ca8ad710af2685c4eba61f7
kanairen <turutekab@gmail.com>
2015-11-17 14:55:59 +0900

    clean clientモジュール呼び出しを行うmain.pyを追加

--- src/client/show_array.py
+++ src/client/show_array.py
@@ -8,7 +8,7 @@ from src.util.visualize import plot_2d
 __author__ = 'ren'
 
 
-def show_np_array(xlabel, ylabel, ylim, location="lower right", keyword=None,
+def show_np_array(x_label, y_label, y_lim, location="lower right", keyword=None,
                   font_size=15, save=False):
     arrays = {}
     for f in os.listdir(path_res_numpy_array):
@@ -17,15 +17,5 @@ def show_np_array(xlabel, ylabel, ylim, location="lower right", keyword=None,
         array = np.load(path_res_numpy_array + "/" + f)
         arrays.setdefault(str(f), array)
 
-    plot_2d(arrays, xlabel, ylabel, locate=location, y_lim=y_lim,
+    plot_2d(arrays, x_label, y_label, locate=location, y_lim=y_lim,
             font_size=font_size, save=save)
-
-
-if __name__ == '__main__':
-    x_label = "iteration"
-    y_label = "accuracy"
-    y_lim = (0, 1)
-    location = 'upper right'
-    keyword = '11-14'
-    show_np_array(x_label, y_label, y_lim, location=location, keyword=keyword,
-                  save=False)
---
+++ src/main.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+
+import sys
+from src.client.image_recognition import image_recognition
+from src.client.show_array import show_np_array
+
+__author__ = 'ren'
+
+if __name__ == '__main__':
+
+    param = sys.argv
+    client = param[1]
+
+    if client == 'image_recognition':
+        image_recognition()
+    elif client == 'show_np_array':
+        x_label = "iteration"
+        y_label = "accuracy"
+        y_lim = (0, 1)
+        location = 'upper right'
+        keyword = '11-14'
+        show_np_array(x_label, y_label, y_lim, location=location,
+                      keyword=keyword)
+
