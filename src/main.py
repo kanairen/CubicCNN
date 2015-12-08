0a7d7075745e38d07df936cc2ec787784aa2f74b
kanairen <turutekab@gmail.com>
2015-12-08 14:15:21 +0900

    main.pyの修正

--- src/main.py
+++ src/main.py
@@ -14,31 +14,33 @@ def main():
     param = sys.argv
     # クライアントタイプ
     client = param[1]
+    # データタイプ
+    data_type = param[2]
+    # 学習繰り返し数
+    n_iter = int(param[3])
+    # バッチ数
+    n_batch = int(param[4])
+    # バッチごとの精度を表示するかどうか
+    show_batch_accuracies = bool(param[5])
+    # バッチごとの精度を保存するかどうか
+    save_batch_accuracies = bool(param[6])
+
+    print "recognizer : ", client
+    print "data_type : ", data_type
+    print "n_iter : ", n_iter
+    print "n_batch : ", n_batch
+    print "show_batch_accuracies : ", show_batch_accuracies
+    print "save_batch_accuracies : ", save_batch_accuracies
 
     if client == 'image_recognition':
-        # データタイプ
-        data_type = param[2]
-        # 学習繰り返し数
-        n_iter = int(param[3])
-        # バッチ数
-        n_batch = int(param[4])
-        # バッチごとの精度を表示するかどうか
-        show_batch_accuracies = bool(param[5])
-        # バッチごとの精度を保存するかどうか
-        save_batch_accuracies = bool(param[6])
-
-        print "data_type : ", data_type
-        print "n_iter : ", n_iter
-        print "n_batch : ", n_batch
-        print "show_batch_accuracies : ", show_batch_accuracies
-        print "save_batch_accuracies : ", save_batch_accuracies
-
         # 画像認識実験
         image_recognition(data_type, n_iter, n_batch,
                           show_batch_accuracies, save_batch_accuracies)
+
     elif client == 'solid_recognition':
-        data_type = param[2]
-        solid_recognition(data_type=data_type)
+        # 三次元形状認識実験
+        solid_recognition(n_iter, n_batch, show_batch_accuracies,
+                          save_batch_accuracies)
 
 
 if __name__ == '__main__':
