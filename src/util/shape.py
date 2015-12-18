a84441c5834f3d8e90444db206c82101814b2089
kanairen <turutekab@gmail.com>
2015-12-18 17:43:31 +0900

    fix 三角関数の引数を修正 in data3d.py

--- src/util/shape.py
+++ src/util/shape.py
@@ -8,18 +8,18 @@ __author__ = 'Ren'
 def rotate_3d(points, r, rotate_priority=[0, 1, 2]):
     assert len(r) == 3
 
-    rx, ry, rz = r
-
     points = np.array(points)
 
+    r_x, r_y, r_z = np.asarray(r, dtype=np.float32) / 180. * np.pi
+
     mtr_x = np.array([[1., 0., 0.],
-                      [0., np.cos(rx), np.sin(rx)],
-                      [0., -np.sin(rx), np.cos(rx)]])
-    mtr_y = np.array([[np.cos(ry), 0., -np.sin(ry)],
+                      [0., np.cos(r_x), np.sin(r_x)],
+                      [0., -np.sin(r_x), np.cos(r_x)]])
+    mtr_y = np.array([[np.cos(r_y), 0., -np.sin(r_y)],
                       [0., 1., 0.],
-                      [np.sin(ry), 0., np.cos(ry)]])
-    mtr_z = np.array([[np.cos(rz), np.sin(rz), 0.],
-                      [-np.sin(rz), np.cos(rz), 0.],
+                      [np.sin(r_y), 0., np.cos(r_y)]])
+    mtr_z = np.array([[np.cos(r_z), np.sin(r_z), 0.],
+                      [-np.sin(r_z), np.cos(r_z), 0.],
                       [0., 0., 1.]])
 
     mtr_a, mtr_b, mtr_c = np.array((mtr_x, mtr_y, mtr_z))[rotate_priority]
