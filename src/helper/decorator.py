425c7ce2e2d44c1867a6e4232ce64a2d0f9218f9
kanairen <turutekab@gmail.com>
2016-01-07 19:44:11 +0900

    add 現在日時出力追加
    src/helper/decorator.py:client()

--- src/helper/decorator.py
+++ src/helper/decorator.py
@@ -1,13 +1,14 @@
 # coding:utf-8
 
 import time
+from src.util.date import ymdt
 
 __author__ = 'ren'
 
 
 def client(func):
     def timer_func(**kwargs):
-        print "Run Client..."
+        print "{} Run Client...".format(ymdt())
         start = time.clock()
         func(**kwargs)
         stop = time.clock()
