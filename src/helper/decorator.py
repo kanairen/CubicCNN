713e215994cc649d85dee087e118f1f6bcd6a5f3
ren <turutekab@gmail.com>
2016-02-18 16:49:32 +0900

    fix decorator not to change function name and docstring.

--- src/helper/decorator.py
+++ src/helper/decorator.py
@@ -1,16 +1,26 @@
 # coding:utf-8
 
 import time
+import functools
 from src.util.date import ymdt
 
 __author__ = 'ren'
 
 
 def client(func):
-    def timer_func(**kwargs):
+    """
+    Client関数・メソッドが呼び出された時の処理
+    :param func: 呼び出し関数
+    :return: デコレータ関数
+    """
+
+    @functools.wraps(func)
+    def timer_func(*args, **kwargs):
         print "{} Run Client...".format(ymdt())
         start = time.clock()
-        func(**kwargs)
+
+        func(*args, **kwargs)
+
         stop = time.clock()
         print "process time : ", stop - start, " s"
 
