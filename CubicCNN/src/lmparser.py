acc8843bc314b2a53da9b14321ace23060e99eef
kanairen <turutekab@gmail.com>
2016-04-08 17:52:01 +0900

    fix travis.ymlの環境変数を修正

---
+++ CubicCNN/src/lmparser.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+
+from collections import OrderedDict
+from model.layer.__hidden import HiddenLayer
+from model.layer.__conv import ConvLayer2d
+from model.layer.__pool import MaxPoolLayer2d
+from model.layer.__softmax import SoftMaxLayer
+from util import strutil
+
+# lmparser.py called as singleton object.
+
+# key
+KEY_DATA = 'data'
+
+KEY_HIDDEN = 'hidden'
+KEY_CONV = 'conv'
+KEY_POOL = 'pool'
+KEY_SOFTMAX = 'softmax'
+
+
+def parse(lm_file):
+    items = []
+    with open(lm_file) as f:
+        while True:
+            line = f.readline()
+            if line == '':
+                break
+            elif line[0] == '#':
+                continue
+
+            sline = line.strip()
+            if sline == '':
+                continue
+            elif '(' == sline[-1]:
+                item_type = sline.strip('(')
+                attr_lines = []
+                line = f.readline().strip()
+                while ')' != line:
+                    attr_lines.append(line)
+                    line = f.readline().strip()
+                items.append((item_type, attr_lines))
+
+    layers = __parse_layers(items)
+    print layers
+
+
+def __parse_layers(items):
+    layers = []
+    for index, item in enumerate(items):
+        item_type, attr_lines = item
+        kwargs = parse_attrs(attr_lines)
+        if item_type == KEY_HIDDEN:
+            layer = HiddenLayer(layer_id=index, **kwargs)
+        elif item_type == KEY_CONV:
+            layer = ConvLayer2d(layer_id=index, **kwargs)
+        elif item_type == KEY_POOL:
+            layer = MaxPoolLayer2d(layer_id=index, **kwargs)
+        elif item_type == KEY_SOFTMAX:
+            layer = SoftMaxLayer(layer_id=index, **kwargs)
+        else:
+            continue
+        layers.append(layer)
+    return layers
+
+
+def parse_attrs(attr_lines):
+    kwargs = {}
+    for line in attr_lines:
+        k, v = line.strip().split('=')
+        kwargs[k] = strutil.toany(v)
+    return kwargs
+
+
+if __name__ == '__main__':
+    import os
+
+    print os.path.abspath(os.curdir)
+    print parse('../../temp.lm')
---
+++ CubicCNN/src/main.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+from model.layer.__hidden import HiddenLayer
+from model.layer.__conv import ConvLayer2d
+from model.layer.__pool import MaxPoolLayer2d
+from model.layer.__softmax import SoftMaxLayer
+from data import image
+from model.model import Model
+from optimizer import Optimizer
+
+from util import calcutil
+
+
+def main():
+    d = image.mnist()
+
+    def layer_gen():
+        l1 = ConvLayer2d(layer_id=0, image_size=d.data_shape,
+                         activation=calcutil.relu, c_in=1, c_out=16, k=(2, 2),
+                         s=(1, 1))
+        l2 = MaxPoolLayer2d(layer_id=1, image_size=l1.output_image_size,
+                            activation=calcutil.identity, c_in=16, k=(2, 2))
+        l3 = ConvLayer2d(layer_id=2, image_size=l2.output_image_size,
+                         activation=calcutil.relu, c_in=16, c_out=32, k=(2, 2),
+                         s=(1, 1))
+        l4 = MaxPoolLayer2d(layer_id=3, image_size=l3.output_image_size,
+                            activation=calcutil.identity, c_in=32, k=(2, 2))
+        l5 = HiddenLayer(layer_id=4, n_in=l4.n_out, n_out=800,
+                         activation=calcutil.relu)
+        l6 = HiddenLayer(layer_id=5, n_in=l5.n_out, n_out=100,
+                         activation=calcutil.relu)
+        l7 = SoftMaxLayer(layer_id=6, n_in=l6.n_out, n_out=len(d.classes()))
+        layers = [l1, l2, l3, l4, l5, l6, l7]
+        return layers
+
+    m = Model(layer_gen)
+    optimizer = Optimizer(d, m)
+    optimizer.optimize(10)
+
+
+if __name__ == '__main__':
+    main()
