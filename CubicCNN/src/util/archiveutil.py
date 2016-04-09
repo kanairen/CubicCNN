25e685c6b4d14e769325e4a7e0e807a133e28b53
kanairen <turutekab@gmail.com>
2016-04-09 15:46:59 +0900

    fix アーカイブユーティリティモジュールのZIP解凍関数を修正

--- CubicCNN/src/util/archiveutil.py
+++ CubicCNN/src/util/archiveutil.py
@@ -7,7 +7,8 @@ import zipfile
 
 def unzip(from_path, to_path):
     with zipfile.ZipFile(from_path, 'r') as zip_file:
+        if not os.path.exists(to_path):
+            os.makedirs(to_path)
         for f in zip_file.namelist():
             with open(os.path.join(to_path, f), 'wb') as unzip_file:
                 unzip_file.write(zip_file.read(f))
-
