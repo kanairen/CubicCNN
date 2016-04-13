e8848453e25e5fe2c50df002204a53aa5b220a41
kanairen <turutekab@gmail.com>
2016-04-13 22:01:47 +0900

    add 3DのConvLayerを追加

--- CubicCNN/src/model/layer/__conv.py
+++ CubicCNN/src/model/layer/__conv.py
@@ -4,8 +4,8 @@
 import numpy as np
 from theano import config, shared
 from theano.tensor.nnet.conv import conv2d
-from theano.sandbox.cuda.fftconv import conv2d_fft,conv3d_fft
-from __grid import GridLayer2d
+from theano.sandbox.cuda.fftconv import conv2d_fft, conv3d_fft
+from __grid import GridLayer2d, GridLayer3d
 
 
 class ConvLayer2d(GridLayer2d):
@@ -50,3 +50,45 @@ class ConvLayer2d(GridLayer2d):
     def __str__(self):
         return super(ConvLayer2d, self).__str__()
 
+
+class ConvLayer3d(GridLayer3d):
+    def __init__(self, layer_id, shape_size, activation, c_in, c_out, k,
+                 s=(1, 1, 1), p=(0, 0, 0), filters=None, b=None,
+                 border_mode='valid', dtype=config.floatX, is_dropout=False,
+                 dropout_rate=0.5):
+        super(ConvLayer3d, self).__init__(layer_id, shape_size, c_in, c_out, k,
+                                          s, p, activation, is_dropout,
+                                          dropout_rate, cover_all=False)
+
+        if filters is None:
+            f_in = c_in * np.prod(self.k)
+            f_out = c_out * np.prod(self.k)
+            w_bound = np.sqrt(6. / (f_in + f_out))
+            shape = [c_out, c_in] + list(reversed(self.k))
+            filters = np.asarray(self.rnd.uniform(low=-w_bound,
+                                                  high=w_bound,
+                                                  size=shape),
+                                 dtype=dtype)
+        self.filters = shared(filters, name='filters{}'.format(layer_id),
+                              borrow=True)
+
+        if b is None:
+            b = np.zeros((c_out,), dtype=dtype)
+        self.b = shared(b, name='b{}'.format(layer_id), borrow=True)
+
+        self.params = self.filters, self.b
+
+        self.border_mode = border_mode
+
+    def output(self, input, is_train):
+        input = super(ConvLayer3d, self).output(input, is_train)
+
+        shape = [self.c_out, self.c_in] + list(reversed(self.k))
+        u = conv3d_fft(input, self.filters,
+                       filter_shape=shape,
+                       border_mode=self.border_mode)
+        # TODO バイアスの挿入位置がここでほんとうに正しいのかテスト
+        return self._activate(u + self.b.dimshuffle('x', 0, 'x', 'x'), is_train)
+
+    def __str__(self):
+        super(ConvLayer3d, self).__str__()
