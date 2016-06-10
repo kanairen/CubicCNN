1569e56ee9f1e02ec4ea0eb7f98ca6338f4b5b5d
kanairen <turutekab@gmail.com>
2016-06-10 16:02:19 +0900

    add SHRECデータセットテスト用関数追加

--- CubicCNN/tests/data/test_shape.py
+++ CubicCNN/tests/data/test_shape.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
+from CubicCNN.src.data.shape import SHRECVoxel
+from CubicCNN.src.util.plotutil import plot_voxel
+from CubicCNN.src.util.parseutil import parse_cla
+from CubicCNN import PATH_RES_SHAPE_SHREC_CLASS_TEST_CLA
 
+
+def test_shrec_voxel():
+    """
+    unittestを使わないSHRECVoxelの簡易テスト
+    :return:
+    """
+    # TODO:SHRECデータセットをダウンロード・binvox変換の機構ができたら適宜unittest対応
+    classes = parse_cla(PATH_RES_SHAPE_SHREC_CLASS_TEST_CLA).keys()
+    shrec = SHRECVoxel.create(n_fold=6)
+    for x, y in zip(shrec.x_train, shrec.y_train):
+        print classes[y]
+        plot_voxel(x[0])
